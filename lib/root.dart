8edb82a6a53fc7d883590857163abcdc0d388ba3
Si Yujun <siuj0810@gmail.com>
2024-07-06 23:02:56 +0900

    Sign In UI 구현 완료 !

---
+++ lib/root.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:hotel_reservation/screens/sign_in.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sign In',
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => SignIn(),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/sign_in.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hotel_reservation/controllers/sign_in_controller.dart';
+import 'package:hotel_reservation/widgets/button/custom_elevated_button.dart';
+import 'package:hotel_reservation/widgets/button/sign_text_button.dart';
+import 'package:hotel_reservation/widgets/text_form_field/sign_text_form_field.dart';
+
+class SignIn extends StatelessWidget {
+  final signInController = Get.put(SignInController());
+  SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  top: 120,
+                ),
+                child: Image(
+                  image: AssetImage('assets/images/hotel_logo.png'),
+                  height: 100,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'HOTEL',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              SignTextFormField(
+                controller: signInController.nameController,
+                isHideText: false,
+                name: 'Username',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SignTextFormField(
+                controller: signInController.passwordController,
+                isHideText: true,
+                name: 'Password',
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SignTextButton(
+                    title: 'CANCEL',
+                    fontsize: 17,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  SignTextButton(
+                    title: 'Sign Up',
+                    fontsize: 17,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CustomElevatedButton(
+                    title: 'NEXT',
+                    fontsize: 17,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
